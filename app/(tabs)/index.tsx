f12d672683ed42b6dc37b3210b3ae007fbdaa517
milena579 <milecalegari@gmail.com>
2024-12-03 13:58:19 -0300

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+   <>
+
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+});
