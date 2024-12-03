86c03564d8e16c54cbb1009f30ea06b8ddf35b24
milena579 <milecalegari@gmail.com>
2024-12-03 17:04:29 -0300

    começando a consumir api hp

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,109 +1,99 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { Image, StyleSheet, Text, View, FlatList, TextInput, Button, ActivityIndicator } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+interface Character {
+  id: string;
+  name: string;
+  image: string;
+}
 
-export default function TabTwoScreen() {
+export default function Explore() {
+  const [character, setCharacter] = useState<Character[]>([]);
+
+  const fetchCharacters = async () => {
+    try {
+      const response =  await axios.get(`https://hp-api.onrender.com/api/characters`);
+      setCharacter(response.data.results);
+
+    } catch (error) {
+      console.error("Erro ao buscar personagens", error)
+      
+    } 
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, [])
+  
+  const renderCharacter = ( {item} : {item:Character} ) => (
+    <View style={styles.card}>
+      <Image source={{uri : item.image}} style={styles.image}/>
+      <View style={styles.info}>
+        <Text style={styles.nome}>{item.name}</Text>
+      </View>
+    </View>
+  )
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+   <>
+    <View style={{ flex: 1 }}>
+      <FlatList data={character} keyExtractor={(item) => item.id.toString()} renderItem={renderCharacter} contentContainerStyle={styles.list}/>
+    </View>
+   </>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  card: {
+    flexDirection : "row",
+    backgroundColor:"#f9f9f9",
+    marginBottom: 12,
+    borderRadius: 8,
+    overflow: "hidden",
+    elevation: 2, //sombra pra android
+    shadowColor: "#000000", //abaixo set de sombra para IOS
+    shadowOpacity: 0.1,
+    shadowOffset: {width: 0,  height:2},
+    shadowRadius: 8
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  info: {
+    flex: 1,
+    padding: 12,
+    justifyContent: "center"
   },
+  image: {
+    width: 100,
+    height: 100
+  },
+  nome :{
+    fontSize: 16,
+    fontWeight :"bold"
+  },
+  status :{
+    fontSize: 14,
+    color: "#00000"
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+  }, 
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding:16,
+    backgroundColor: "#f0f0f0"
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor:"#cccccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginRight: 8
+  },
+  list: {
+    padding: 16
+  }
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,119 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Text, View, FlatList, TextInput, Button, ActivityIndicator } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
+interface Character {
+  id: number;
+  name: string;
+  image: string;
+  status: string
+}
 
 export default function HomeScreen() {
+  const [character, setCharacter] = useState<Character[]>([]);
+  const [loading, setLoad] = useState<boolean>(true);
+  const [page, setPage] = useState<string>("1");
+
+  const fetchCharacters = async (pageNumber: string) => {
+    try {
+      const response =  await axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`);
+      setCharacter(response.data.results);
+
+    } catch (error) {
+      console.error("Erro ao buscar personagens", error)
+      
+    } finally{
+      setLoad(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacters(page);
+  }, [page])
+  
+  const renderCharacter = ( {item} : {item:Character} ) => (
+    <View style={styles.card}>
+      <Image source={{uri : item.image}} style={styles.image}/>
+      <View style={styles.info}>
+        <Text style={styles.nome}>{item.name}</Text>
+        <Text style={styles.status}>{item.status}</Text>
+      </View>
+    </View>
+  )
+
+  if(loading){
+    return (
+      <>
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="#ADF7C3FF"/>
+        </View>
+      </>
+    )
+  }
   return (
    <>
-
+    <View style={{ flex: 1 }}>
+      <View style={styles.inputContainer}>
+        <Text>1/42 - </Text>
+        <TextInput style={styles.input} value={page} onChangeText={(text) => setPage(text)} keyboardType='numeric' placeholder='Digite o número da página'/>
+        <Button title='Buscar' onPress={() => fetchCharacters(page)} />
+      </View>
+      <FlatList data={character} keyExtractor={(item) => item.id.toString()} renderItem={renderCharacter} contentContainerStyle={styles.list}/>
+    </View>
    </>
   );
 }
 
 const styles = StyleSheet.create({
+  card: {
+    flexDirection : "row",
+    backgroundColor:"#f9f9f9",
+    marginBottom: 12,
+    borderRadius: 8,
+    overflow: "hidden",
+    elevation: 2, //sombra pra android
+    shadowColor: "#000000", //abaixo set de sombra para IOS
+    shadowOpacity: 0.1,
+    shadowOffset: {width: 0,  height:2},
+    shadowRadius: 8
+  },
+  info: {
+    flex: 1,
+    padding: 12,
+    justifyContent: "center"
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
+  nome :{
+    fontSize: 16,
+    fontWeight :"bold"
+  },
+  status :{
+    fontSize: 14,
+    color: "#00000"
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+  }, 
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding:16,
+    backgroundColor: "#f0f0f0"
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor:"#cccccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginRight: 8
+  },
+  list: {
+    padding: 16
+  }
 });
